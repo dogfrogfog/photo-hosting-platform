fbd3534c8a76dea1e5f8f995d0dde5de92d7167f
maksim hodasevich <maksim.hodasevich@gmail.com>
2023-11-21 22:02:53 +0100

    add primary keys

--- db/schema.ts
+++ db/schema.ts
@@ -1,14 +1,14 @@
 import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const group = pgTable("group", {
-  id: serial("id"),
+  id: serial("id").primaryKey(),
   name: text("name"),
   createdAt: timestamp("created_at"),
   updatedAt: timestamp("updated_at"),
 });
 
 export const user = pgTable("user", {
-  id: serial("id"),
+  id: serial("id").primaryKey(),
   clerkId: text("clerk_id"),
   firstName: text("first_name"),
   lastName: text("last_name"),
