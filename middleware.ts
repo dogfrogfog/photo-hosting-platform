1e5a3faa178820ff7f556450f2f5f0ced5f3eb32
maksim hodasevich <maksim.hodasevich@gmail.com>
2023-12-16 10:56:23 +0100

    fix typo

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/gallery", "/api/webhooks"],
+  publicRoutes: ["/", "/gallery", "/api/webhook"],
 });
 
 export const config = {
